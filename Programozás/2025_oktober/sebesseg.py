dd577f288618a00b2d985c5ebd87cac5813cbf05
Istvan Szalontai <istvan.szalontai12@gmail.com>
2026-01-27 13:51:59 +0100

    3rd, 4th and 5th solution added to sebesseg.py task

--- Programozás/2025_oktober/sebesseg.py
+++ Programozás/2025_oktober/sebesseg.py
@@ -1,28 +1,69 @@
 print("3. Sebességes feladat")
 print("1. feladat")
 f = open("ut.txt")
-össz_hossz = f.readline()
+össz_hossz = float(f.readline())
 print("Teljes útszakasz:", össz_hossz, type(össz_hossz))
 
 útszakasz = []
 for sor in f:
   adatok = sor.split()
   rekord = {
-    "km":   adatok[0],
+    "km":   int(adatok[0]),
     "adat": adatok[1]
   }
   útszakasz.append(rekord)
 
+print()
 print("2. feladat")
 print("A települések neve:")
 for sor in útszakasz:
-  if len(sor["adat"])>=4:
+  if len(sor["adat"]) >= 4:
     print(sor["adat"])
-
+    
+print()
 print("3. feladat")
-vizsgált = input("Adja meg a vizsgált szakasz hosszát km-ben! ")
-
-
+vizsgált = float(input("Adja meg a vizsgált szakasz hosszát km-ben! ")) * 1000
+minimum = 9999
+for sor in útszakasz:
+    if sor["km"] > vizsgált:
+        break
+    # print(sor["km"], "-->", sor["adat"])
+    if len(sor["adat"]) == 2:
+        minimum = min(int(sor["adat"]), minimum)
+    if len(sor["adat"]) >= 4:
+        minimum = min(50, minimum)
+print(f"Az első {vizsgált/1000} km-en {minimum} km/h volt a legalacsonyabb megengedett sebesség.")
+    
+print()
+print("4. feladat")
+összeg = 0
+város_kezdete = 0
+for sor in útszakasz:
+    if len(sor["adat"]) >= 4:
+        város_kezdete = float(sor["km"])
+    if sor["adat"] == "]":
+        város_vége = float(sor["km"])
+        összeg += (város_vége - város_kezdete)
+százalék = (összeg/össz_hossz)*100
+print(f"Az út {százalék} százaléka vezet településen belül.")
+    
+print()
+print("5. feladat")
+település = input("Adja meg egy település nevét! ")
+van_e = False
+darab = 0
+for sor in útszakasz:
+    if sor["adat"] == település:
+        van_e = True
+    if van_e == True :
+        darab
+    
+    
+    
+    
+    
+    
+    
 
 
 
