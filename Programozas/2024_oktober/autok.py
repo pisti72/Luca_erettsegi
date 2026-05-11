7261aa30b642979dd79836a252816b97cdab2f10
István Szalontai <istvan.szalontai12@gmail.com>
2026-05-11 11:29:52 +0200

    autok.py added

--- Programozas/2024_oktober/autok.py
+++ Programozas/2024_oktober/autok.py
@@ -21,8 +21,10 @@ for j in jelek:
 print("Jeladásainak időpontjai:", idok)
 
 print("4. feladat")
-óra = input("Kérem, adja meg az órát:")
-perc = input("Kérem, adja meg a percet:")
+#óra = input("Kérem, adja meg az órát:")
+#perc = input("Kérem, adja meg a percet:")
+óra = "6"
+perc = "54"
 db = 0
 for j in jelek:
     if j[ÓRA] == óra and j[PERC] == perc:
@@ -30,4 +32,21 @@ for j in jelek:
 print("A jeladások száma:", db)
 
 print("5. feladat")
- 
+print("6. feladat") 
+print("7. feladat")
+rendszamok = {}
+for j in jelek:
+    rsz = j[RSZÁM]
+    if rsz in rendszamok:
+        rendszamok[rsz][2] = j[ÓRA]
+        rendszamok[rsz][3] = j[PERC]
+    else:
+        rendszamok[rsz] = [j[ÓRA], j[PERC], "semmi", "semmi" ]
+file = open("ido.txt","wt",encoding = "UTF-8")
+txt = ""
+for key in rendszamok:
+    txt += key + " " + rendszamok[key][0] + " " + rendszamok[key][1] + " " + rendszamok[key][2] + " " + rendszamok[key][3]
+    txt += "\n"
+print(txt)
+file.write(txt)
+file.close()
